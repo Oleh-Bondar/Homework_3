2802fc4afcf4baad824b3616783536a3a98686ad
Олег Бондар <oleh.bondar@nure.ua>
2021-11-04 12:11:27 +0200

    Final soluthion

--- Source.cpp
+++ Source.cpp
@@ -7,29 +7,28 @@ int main()
     setlocale(0, "");
 
     double x, count;
+    cout << "Введите значение Х для функции sin(x): ";
     cin >> x;
+    cout << "Введите точность вычислений: ";
     cin >> count;
 
-    //xccsc;
     int factorial = 1;
+    int fuctorial_num = 2;
     double powerx = x;
     double result = x;
-    int j = 2;
     for (int i = 0; i < count; i++) {
+        powerx *= x * x;
+        factorial *= fuctorial_num * (fuctorial_num + 1);
             if (i % 2 == 0) {
-                powerx *= x * x;
-                factorial *= j * (j + 1);
                 result -= powerx / factorial;
             }
             else {
-                powerx *= x * x;
-                factorial *= j * (j + 1);
                 result += powerx / factorial;
             }
-
-            j += 2;
+            fuctorial_num += 2;
     }
    
+    cout << "Ваш результат: ";
     cout << result << endl;
 
     system("pause");
