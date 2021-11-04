ce1f85dca8dfdba13e37418cd210fdc687e05076
Олег Бондар <oleh.bondar@nure.ua>
2021-11-04 12:11:54 +0200

    Revert "Final soluthion"
    This reverts commit 2802fc4afcf4baad824b3616783536a3a98686ad.

--- Source.cpp
+++ Source.cpp
@@ -7,28 +7,29 @@ int main()
     setlocale(0, "");
 
     double x, count;
-    cout << "Введите значение Х для функции sin(x): ";
     cin >> x;
-    cout << "Введите точность вычислений: ";
     cin >> count;
 
+    //xccsc;
     int factorial = 1;
-    int fuctorial_num = 2;
     double powerx = x;
     double result = x;
+    int j = 2;
     for (int i = 0; i < count; i++) {
-        powerx *= x * x;
-        factorial *= fuctorial_num * (fuctorial_num + 1);
             if (i % 2 == 0) {
+                powerx *= x * x;
+                factorial *= j * (j + 1);
                 result -= powerx / factorial;
             }
             else {
+                powerx *= x * x;
+                factorial *= j * (j + 1);
                 result += powerx / factorial;
             }
-            fuctorial_num += 2;
+
+            j += 2;
     }
    
-    cout << "Ваш результат: ";
     cout << result << endl;
 
     system("pause");
